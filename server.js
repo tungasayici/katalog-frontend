a63acb84db58465a29a8bf19e1bffa90501d63ed
Yakup AD <yakupad@yandex.com>
2018-05-28 12:34:04 +0300

    production errors fixed-server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/DTKatalog'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname + 'dist/DTKatalog/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/DTKatalog/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
