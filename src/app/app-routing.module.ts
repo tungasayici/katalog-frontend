8339a73e8a7235a942aa428208366a38e75b4607
Yakup AD <yakupad@yandex.com>
2018-05-28 12:20:39 +0300

    production errors fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from "../app/pages/home/home.component";
-import { DetailComponent } from "../app/pages/detail/detail.component";
-import { AddProductComponent } from "../app/pages//add-product/add-product.component";
-import { SearchComponent } from "../app/pages/search/search.component";
+import { HomeComponent } from '../app/pages/home/home.component';
+import { DetailComponent } from '../app/pages/detail/detail.component';
+import { AddProductComponent } from '../app/pages//add-product/add-product.component';
+import { SearchComponent } from '../app/pages/search/search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
--- src/app/interfaces/ProductItem.ts
+++ src/app/interfaces/ProductItem.ts
@@ -1,10 +1,10 @@
-import {Product} from "../models/Product"
-import {Asset} from "../models/Asset";
-import {Consumer} from "../models/Consumer";
-import {InvestmentLog} from "../models/InvestmentLog";
-import {Like} from "../models/Like";
+import {Product} from '../models/Product'
+import {Asset} from '../models/Asset';
+import {Consumer} from '../models/Consumer';
+import {InvestmentLog} from '../models/InvestmentLog';
+import {Like} from '../models/Like';
 
-export interface IProductItem{
+export interface IProductItem {
     product: Product;
     consumer: Consumer;
     investmentLogList: InvestmentLog[];
