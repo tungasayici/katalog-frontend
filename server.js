278e86fe35cb310a67405444c57909c3cda7470a
Yakup AD <yakupad@yandex.com>
2018-05-30 12:03:59 +0300

    server js uplad directory path fixed

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/upload-file', function(req, res, next) {
     if (req.busboy) {
       req.busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
           console.log("server bussby içi")
-        fstream = fs.createWriteStream(__dirname + '/../../public/my-files/' + filename);
+        fstream = fs.createWriteStream(__dirname + '/public/my-files/' + filename);
         file.pipe(fstream);
         fstream.on('close', function(){
           console.log('file ' + filename + ' uploaded');
