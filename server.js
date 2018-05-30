a65fd244f464f1a4f082458b4095bec78b5887da
Yakup AD <yakupad@yandex.com>
2018-05-30 11:41:56 +0300

    morgan package added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var busboy = require('connect-busboy');
 
