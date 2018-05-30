6b37f0a04e8c26fba81cf9187cf2fea561f4887c
Yakup AD <yakupad@yandex.com>
2018-05-30 11:47:15 +0300

    morgan package added

--- server.js
+++ server.js
@@ -18,22 +18,7 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/DTKatalog/index.html'));
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
-  // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 app.post('/upload-file', function(req, res, next) {
     var fstream;
